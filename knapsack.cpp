9ac238b662b8b1c3e2d1ffe8b7c9553a2388af54
Glenn G. Chappell <ggchappell@alaska.edu>
2025-10-31 08:46:03 -0800

    knapsack.cpp: FINISH - write func knapsack_mem

--- knapsack.cpp
+++ knapsack.cpp
@@ -1,4 +1,4 @@
-// knapsack.cpp  UNFINISHED
+// knapsack.cpp
 // Glenn G. Chappell
 // 2025-10-29
 //
@@ -77,16 +77,80 @@ int knapsack_bf(const vector<int> & weights,
 }
 
 
+// Marker for unknown values in array
+const int UNKNOWN = -1;
+
+
+// knapsack_mem_recurse
+// Given lists of object weights & values, knapsack capacity, and n,
+// returns greatest possible value in knapsack problem using only
+// objects from first n.
+// Recursive helper for function knapsack_mem.
+// Values stored in vector knapsacktable, initialized to all UNKNOWN.
+// Value for capacity, n is knapsacktable[capacity*dim+n].
+int knapsack_mem_recurse(const vector<int> & weights,
+                         const vector<int> & values,
+                         int capacity,
+                         size_t n,
+                         vector<int> & knapsacktable,
+                         size_t dim)
+{
+    assert(weights.size() == values.size());
+    assert(n <= weights.size());
+    int result;  // For our final result
+
+    // Check if we have already computed this; if so, do not recompute
+    if (knapsacktable[capacity*dim+n] != UNKNOWN)
+        return knapsacktable[capacity*dim+n];
+
+    if (n == 0)  // BASE CASE
+    {
+        result = 0;
+    }
+    else         // RECURSIVE CASE
+    {
+        int max_without_last =
+            knapsack_mem_recurse(weights, values,
+                                 capacity, n-1,
+                                 knapsacktable, dim);
+        if (weights[n-1] > capacity)
+            result = max_without_last;
+        else
+        {
+            int max_with_last = values[n-1] +
+                knapsack_mem_recurse(weights, values,
+                                     capacity-weights[n-1], n-1,
+                                     knapsacktable, dim);
+            result = max(max_with_last, max_without_last);
+        }
+    }
+
+    // Save our result and return it
+    knapsacktable[capacity*dim+n] = result;
+    return result;
+}
+
+
 // knapsack_mem
 // Given lists of object weights & values, and knapsack capacity,
 // returns greatest possible value in knapsack problem.
 // Memory-function version.
+// Calls recursive helper function knapsack_mem_recurse.
 int knapsack_mem(const vector<int> & weights,
                  const vector<int> & values,
                  int capacity)
 {
-    // TODO: WRITE THIS!!!
-    return 42;  // DUMMY
+    assert(weights.size() == values.size());
+    size_t n = weights.size();
+
+    // Create space for memoized values.
+    // Items start at UNKNOWN, meaning not valid. We store values for
+    // 0..capacity and 0..n; we need dimensions (capacity+1) and (n+1).
+    size_t dim = n+1;
+    vector<int> knapsacktable((capacity+1)*dim, UNKNOWN);
+    return knapsack_mem_recurse(weights, values,
+                                capacity, n,
+                                knapsacktable, dim);
 }
 
 
